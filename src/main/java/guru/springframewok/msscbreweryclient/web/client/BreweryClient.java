0056cef417824268260f36b1c5f03703649f9fdf
Daniel <danielrodrigo098@gmail.com>
2024-06-01 15:41:14 -0300

    First commit

---
+++ src/main/java/guru/springframewok/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,33 @@
+package guru.springframewok.msscbreweryclient.web.client;
+
+import guru.springframewok.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    private String apihost;
+    public final String BEER_PATH_V1 = "api/v1/beer/";
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID id){
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + id.toString(), BeerDto.class);
+    }
+
+    public void setApihost(String apihost) {
+        this.apihost = apihost;
+    }
+
+
+}
---
+++ src/main/java/guru/springframewok/msscbreweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package guru.springframewok.msscbreweryclient.web.client;
